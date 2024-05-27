a82dd52a659eb801bfa630214d7450a6b6ced414
adwini <aclcadwin@gmail.com>
2024-05-27 20:32:56 +0800

    Admin Dashboard is Working with some minor bug in Modal Validation

--- resources/views/livewire/pages/admin/dashboard.blade.php
+++ resources/views/livewire/pages/admin/dashboard.blade.php
@@ -4,11 +4,29 @@ use function Livewire\Volt\{state, computed, Layout};
 use Livewire\Volt\Component;
 use Livewire\Attributes\Layout;
 use App\Models\Branch;
+use App\Livewire\Forms\AddBranchForm;
+
 
 new #[Layout('layouts.app')] class extends Component
 {
      #[state]
     public $branches = [];
+    public bool $addModal = false;
+
+    public $branchName = '';
+    public $branchLoc = '';
+    public $no_of_employee = '';
+
+    public AddBranchForm $form;
+
+    public $headers = [
+
+        ['key' => 'branchName', 'label' => 'Branch Name'],
+        ['key' => 'branchLoc', 'label' => 'Location'],
+        ['key' => 'no_of_employee', 'label' => 'No. of Employee'],
+        // ['key' => 'city.name', 'label' => 'City'] # <---- nested attributes
+    ];
+
 
     // Fetch the branches from the database
     public function mount()
@@ -16,25 +34,46 @@ new #[Layout('layouts.app')] class extends Component
         $this->branches = Branch::all();
     }
 
+     public function save() {
+
+        $validated = $this->validate([
+            'branchName' => 'required|max:255',
+            'branchLoc' => 'required|max:255',
+            'no_of_employee'=>'required|int'
+        ]);
+        Branch::create($validated);
+         $this->reset();
+        $this->addModal = false;
+
+       $this->redirectIntended(default: route('dashboard', absolute: false), navigate: true);
+    }
+
+
+
+
+
  }
 ?>
 
 <div>
     <x-mary-header title="Branches">
         <x-slot:actions>
-            <x-mary-button icon="o-plus" class="btn-primary" tooltip="Add Branch" />
+            <x-mary-button icon="o-plus" class="btn-primary" tooltip="Add Branch" @click="$wire.addModal = true" />
         </x-slot:actions>
     </x-mary-header>
 
-    <div class="overflow-x-auto">
+    {{-- <x-mary-table :headers="$headers" :rows="$branches" striped @row-click="alert($event.detail.name)" /> --}}
+
+
+    {{-- <div class="overflow-x-auto">
         <table class="table">
             <!-- head -->
             <thead>
                 <tr>
 
-                    <th>Branch Name</th>
-                    <th>Branch Location</th>
-                    <th>No. of Employees</th>
+                    <th>Branch Name              </th>
+                    <th>        </th>
+                    <th>No. of Employees         </th>
                 </tr>
             </thead>
             <tbody>
@@ -47,5 +86,59 @@ new #[Layout('layouts.app')] class extends Component
           @endforeach
             </tbody>
         </table>
-    </div>
+    </div> --}}
+
+
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400">
+        <thead class="text-xs text-gray-900 uppercase dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                   Branch Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                   Branch Location
+                </th>
+                <th scope="col" class="px-6 py-3">
+                   No. of Employees
+                </th>
+
+            </tr>
+        </thead>
+        <tbody>
+             @foreach ($branches as $branch)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {{ $branch->branchName }}
+                </th>
+                <td class="px-6 py-4">
+                 {{ $branch->branchLoc }}
+                </td>
+                <td class="px-6 py-4">
+                 {{ $branch->no_of_employee }}
+                </td>
+
+          @endforeach
+        </tbody>
+    </table>
 </div>
+
+
+{{-- Modal for Adding a branch --}}
+<x-mary-modal wire:model="addModal" class="backdrop-blur">
+<x-mary-form wire:submit="save">
+    <x-mary-input label="Branch Name" wire:model="branchName" class=""/>
+    <x-mary-input label="Branch Location" wire:model="branchLoc" />
+    <x-mary-input label="Number of Employees" wire:model="no_of_employee"/>
+
+
+    <x-slot:actions>
+
+    <x-mary-button label="Cancel" @click="$wire.addModal = false" />
+   <x-mary-button label="Add" class="btn-primary" type="submit" spinner="save" />
+    </x-slot:actions>
+</x-mary-form>
+</x-mary-modal>
+
+</div>
+
