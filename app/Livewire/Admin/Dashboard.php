4ea64a7957cd40b92dd4f422fd79cd2362b5e8b2
adwini <aclcadwin@gmail.com>
2024-06-01 00:10:56 +0800

    Dashboard and Modify all goods

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -2,89 +2,91 @@
 
 namespace App\Livewire\Admin;
 
-use App\Livewire\Forms\AddBranchForm;
 use App\Models\Branch;
-use Livewire\Attributes\Layout;
 use Livewire\Attributes\Lazy;
 use Livewire\Component;
+use Livewire\Attributes\Layout;
+use App\Livewire\Forms\AddBranchForm;
 use Livewire\Features\SupportPagination\WithoutUrlPagination;
 use Livewire\WithPagination;
 
-#[Layout('layouts.app')]
 #[Lazy]
-class Dashboard extends Component
 
+#[Layout('layouts.app')]
+class Dashboard extends Component
 {
+       use WithPagination, WithoutUrlPagination;
+
+
+    public AddBranchForm $form;
+    public $search = '';
+    public $page = 10;
+    public $branch;
+    // public $branchName = '';
+    // public $branchLoc = '';
+
+
+
+    public bool $editMode = false;
+
+    public function mount( ){
+
+        $dataFromDb = Branch::all();
+
+    }
+
+
+    public function edit($id){
+        $branch = Branch::find($id );
+        $this->form->setBranch($branch);
+        $this->editMode = true;
+        $this->addModal = true;
+    }
+
+    public function save(){
+
+        if ($this->editMode){
+            $this->form->update();
+            $this->editMode = false;
+        }else{
+        $this->form->store();
+
+
+        }
+
+        $this->addModal = false;
+    }
+
+    public bool $addModal =false;
+    public bool $deleteModal = false;
+
+
+
+        public function cancel()
+        {
+            $this->reset();
+            $this->resetErrorBag();
+            $this->addModal = false;
+             // Clear validation errors
+        }
+
 
     public function placeholder(){
         return view ('skeleton');
     }
-
-   use WithPagination, WithoutUrlPagination;
-    public function mount()
-    {
-        $this->branches = Branch::all();
-    }
     public function render()
     {
-         $headers = [
-        ['key' => 'branchName', 'label' => 'Branch Name'],
-        ['key' => 'branchLoc', 'label' => 'Location'],
-        ['key' => 'no_of_employee', 'label' => 'No. of Employee'],
-        // ['key' => 'city.name', 'label' => 'City'] # <---- nested attributes
-    ];
 
-        return view('livewire.admin.dashboard', [
-            // 'branch'=> Branch::paginate($this->perPage),
-            // 'branches'=> Branch::all(),
-            'branch_pag' =>Branch::paginate(10),
-
-            'header'=> $headers,
+        return view('livewire.admin.dashboard',[
 
         ]);
     }
 
-    // public $perPage = 10;
-     public $branches = [];
-    public bool $addModal = false;
+    public function delete($branchId){
 
-    // public $branch_pag =[];
-
-    public $search = '';
-
-    public $branchName = '';
-    public $branchLoc = '';
-    public $no_of_employee = '';
-
-    public AddBranchForm $form;
-
-
-     public function save() {
-        $this->form->store();
-        $this->addModal = false;
-        $this-> redirect('/dashboard', navigate:true);
-     }
-
-
-     public function resetForm()
-    {
-        $this->reset(['branchLoc','branchName','no_of_employee']);
-
-        $this->resetErrorBag();
-    }
-     public function cancel()
-    {
-        $this->resetForm();
-        $this->addModal = false;
+        $branch = Branch::findOrFail($branchId);
+         $branch->delete();
+        return $this->redirect('/branch-list', navigate:true);
     }
 
-    public function updatedAddModal($value)
-    {
-        if (!$value) {
-            $this->resetForm();
-            $this->resetErrorBag();
-        }
-    }
-
- }
-
+}
--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,78 +1,81 @@
+<?php
+
+    use App\Models\Branch;
+
+    $search = Branch::search($this->search)->paginate($this->page);
+
+
+   $headers = [
+        ['key' => 'branchName', 'label' => 'BRANCH NAME'],
+        ['key' => 'branchLoc', 'label' => 'LOCATION'],
+        ['key' => 'no_of_employee', 'label' => 'NUMBER OF EMPLOYEE'],
+        ['key' => 'no_of_token_available', 'label' => 'TOTAL TOKEN'],
+
+        // ['key' => 'city.name', 'label' => 'City'] # <---- nested attributes
+    ];
+?>
 
 <div>
-    <x-mary-header title="Dashboard" class="font-medium text-xxs">
-         <x-slot:middle class="!justify-end">
-            <x-mary-input icon="o-magnifying-glass" placeholder="Search..." clearable wire:model.live="search"  />
-         {{-- <x-mary-input  placeholder="sasadasdasd..."   /> --}}
+    <x-mary-header title="Dashboard">
+        <x-slot:middle class="!justify-end">
+         <x-mary-input icon="o-magnifying-glass" wire:model.live="search" clearable placeholder="Search..." />
      </x-slot:middle>
-
         <x-slot:actions>
             <x-mary-button icon="o-plus" class="btn-primary" tooltip="Add Branch" @click="$wire.addModal = true" />
         </x-slot:actions>
     </x-mary-header>
 
 
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                   <th scope="col" class="px-6 py-3">
-            Branch Name
-        </th>
-        <th scope="col" class="px-6 py-3">
-            Branch Location
-        </th>
-        <th scope="col" class="px-6 py-3">
-            Number of Employees
-        </th>
-        <th scope="col" class="px-6 py-3">
-           Total Tokens
-        </th>
-            </tr>
-        </thead>
-        <tbody>
-              @foreach ($branch_pag as $branch)
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {{ $branch->branchName }}
-                </th>
-                <td class="px-6 py-4">
-                   {{ $branch->branchLoc }}
-                </td>
-                <td class="px-6 py-4">
-                   {{ $branch->no_of_employee }}
-                </td>
-                <td class="px-6 py-4">
-                    {{ $branch->no_of_token_available }}
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
+ <x-mary-table :headers="$headers" :rows="$search" :row-decoration striped with-pagination>
+
+    @scope('header_branchName', $header)
+    <h2 class="text-sm font-bold text-gray-350">
+    {{ $header['label'] }}
+        </h2>
+    @endscope
+    @scope('header_branchLoc', $header)
+    <h2 class="text-sm font-bold text-gray-350">
+            {{ $header['label'] }}
+        </h2>
+    @endscope
+    @scope('header_no_of_employee', $header)
+        <h2 class="text-sm font-bold text-gray-350">
+            {{ $header['label'] }}
+        </h2>
+    @endscope
+    @scope('header_no_of_token_available', $header)
+        <h2 class="text-sm font-bold text-gray-350">
+            {{ $header['label'] }}
+        </h2>
+    @endscope
+
+</x-mary-table>
+    {{-- Add Modal --}}
+    <x-mary-modal wire:model="addModal" persistent class="backdrop-blur">
+    <x-mary-form wire:submit.prevent="save">
+        <x-mary-input label="Branch Name" wire:model="form.branchName" class=""/>
+        <x-mary-input label="Branch Location" wire:model="form.branchLoc" />
+        <x-mary-input label="Number of Employees" wire:model="form.no_of_employee"/>
 
 
-    </table>
+        <x-slot:actions>
 
-</div>
-
-      <div class="mt-4 mb-2" >
-        {{ $branch_pag->links() }}
-    </div>
-
-
-{{-- Modal for Adding a branch --}}
-<x-mary-modal wire:model="addModal" persistent class="backdrop-blur">
-<x-mary-form wire:submit.prevent="save">
-    <x-mary-input label="Branch Name" wire:model="form.branchName" class=""/>
-    <x-mary-input label="Branch Location" wire:model="form.branchLoc" />
-    <x-mary-input label="Number of Employees" wire:model="form.no_of_employee"/>
-
-
-    <x-slot:actions>
-
-<x-mary-button label="Cancel" @click="$wire.call('cancel')" />
+    <x-mary-button label="Cancel" link="/branch-list" />
     <x-mary-button label="Add" class="btn-primary" type="submit" spinner="save" />
-    </x-slot:actions>
-</x-mary-form>
-</x-mary-modal>
+        </x-slot:actions>
+    </x-mary-form>
+    </x-mary-modal>
+
+
+    {{-- Delete Modal --}}
+
+    {{-- <div class="mt-4 mb-2" >
+        {{ $search->links() }}
+    </div> --}}
+
+{{-- Delete Modal  --}}
+
+
 
 </div>
+
