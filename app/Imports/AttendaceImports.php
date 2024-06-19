cad19a48c8172d43b27d26808210f10a04a807bf
JanB <cs31a8balbon@gmail.com>
2024-06-20 07:46:58 +0800

    testing for upload excel file

--- app/Imports/AttendaceImports.php
+++ app/Imports/AttendaceImports.php
@@ -15,7 +15,12 @@ class AttendaceImports implements ToModel
     public function model(array $row)
     {
         return new AttandanceImport([
-            //
+            'name' => $row[0],
+            'date_hired' => $row[1],
+            'sick_leave' => $row[2],
+            'vacation_leave' => $row[3],
+            'awol' => $row[4],
+            'total_absents' => $row[5],
         ]);
     }
 }
--- app/Livewire/Employee/EmpVoting.php
+++ app/Livewire/Employee/EmpVoting.php
@@ -4,6 +4,9 @@ namespace App\Livewire\Employee;
 
 use Livewire\Component;
 use Livewire\Attributes\Layout;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\AttendaceImports;
+use Exception;
 
 #[Layout('layouts.employee')]
 class EmpVoting extends Component
@@ -11,5 +14,18 @@ class EmpVoting extends Component
     public function render()
     {
         return view('livewire.employee.emp_voting');
+    }
+
+    public $attandance_import = 'attendance.xlsx';
+
+    public function import() {
+        
+        try{
+            Excel::import(new AttendaceImports, $this->attandance_import);
+            dd('Okay!');
+        } catch (Exception $e){
+            dd($e);
+        }
+        // return redirect('/')->with('success', 'All Good!');
     }
 }
